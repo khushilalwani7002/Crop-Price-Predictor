d3dfa5da3bcb82e2a8fedd0bbcae86a0a1189546
nikitakumar-beep <nikita.kumar@bscedh.christuniversity.in>
2026-03-24 15:17:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-pip.install Flask requests jsonify CORS pickle
-from flask import Flask, requests, jsonify
+from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
 from flask_cors import CORS # This allows your HTML to talk to this Python script
